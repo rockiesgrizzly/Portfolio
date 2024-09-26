3902e3402295d894375d254e5d95b6107e0760c1
Josh MacDonald <rockiesgrizzly@icloud.com>
2024-09-25 20:18:22 -0600

    UIKit subviews

--- Portfolio/Presentation/GenAi/View UIKit/GenAiUiContentView.swift
+++ Portfolio/Presentation/GenAi/View UIKit/GenAiUiContentView.swift
@@ -14,6 +14,16 @@ class GenAiUiContentView: UIView {
         }
     }
     
+    private let topLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private let textField: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
+    
     // MARK: - Lifecycle
     init(viewModel: GenAiViewModel, frame: CGRect = .zero) {
         self.viewModel = viewModel
@@ -26,8 +36,35 @@ class GenAiUiContentView: UIView {
     }
     
     // MARK: - Private
-    private func update(with viewModel: GenAiViewModel) {
+    private func setupSubviews() {
+        textField.delegate = self
         
+        addSubview(topLabel)
+        addSubview(textField)
+        
+        topLabel.translatesAutoresizingMaskIntoConstraints = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            topLabel.topAnchor.constraint(equalTo: topAnchor, constant: 24),
+            topLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            topLabel.bottomAnchor.constraint(equalTo: textField.topAnchor, constant: 16),
+            textField.centerXAnchor.constraint(equalTo: centerXAnchor),
+            textField.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16)
+        ])
+    }
+    
+    private func update(with viewModel: GenAiViewModel) {
+        topLabel.text = viewModel.userInvitationText
+        textField.placeholder = viewModel.promptDefaultText
     }
 }
 
+extension GenAiUiContentView: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        guard let prompt = textField.text else { return }
+        Task {
+            try await viewModel.respond(toPrompt: prompt)
+        }
+    }
+}
--- Portfolio/Presentation/GenAi/View UIKit/GenAiViewController.swift
+++ Portfolio/Presentation/GenAi/View UIKit/GenAiViewController.swift
@@ -8,9 +8,25 @@
 import UIKit
 
 class GenAiViewController: UIViewController {
+    private var viewModel = GenAiViewModel()
     
+    // MARK: - Lifecycle
+    convenience init() {
+        self.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        assertionFailure("GenAiViewController required init?(coder... not implemented")
+        return nil
+    }
+    
+    override func loadView() {
+        let genAiUiView = GenAiUiView(viewModel: viewModel)
+        view = genAiUiView
+    }
 }
 
+// MARK: - SwiftUI UIViewControllerRepresentable conformance
 import SwiftUI
 
 struct GenAiUiViewRepresentable: UIViewControllerRepresentable {
